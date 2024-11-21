c61a3c75d1be0c493f91bc38df22f6d9d2e46ddd
Yash Arya <yasharya2901@gmail.com>
2024-11-21 12:52:18 +0530

    About Page about to complete

---
+++ src/app/components/About/About.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='flex flex-col'>
+        <div className='text-[#4ADE80]'>Hello World! I'm</div>
+        <div className='text-white font-extrabold text-6xl'>Yash Arya</div>
+        <div className='text-[#A3A3A3]'>Backend Engineer && Problem Solver</div>
+    </div>
+  )
+}
+
+export default About
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div className="w-full h-28 bg-black">
+    <div className="w-full h-full bg-black">
       <Navbar/>
+      <About/>
     </div>
   );
 }
