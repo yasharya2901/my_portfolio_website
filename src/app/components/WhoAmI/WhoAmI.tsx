0a61d93c1a201dbfd1162c8033dda673de88a1c9
Yash Arya <yasharya2901@gmail.com>
2024-11-22 11:35:11 +0530

    CodeBlock Function Completed

--- src/app/components/WhoAmI/WhoAmI.tsx
+++ src/app/components/WhoAmI/WhoAmI.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 function WhoAmI() {
   return (
     <div className='flex flex-row'>
-        <div className='flex flex-col'>
+        <div className='flex flex-col gap-4'>
             <div className='text-[#4ADE80]'>/* about_me.go */</div>
-            <div>
+            <div className='bg-[#171717] p-8 pr-32 rounded-xl'>
                 <CodeBlock/>
             </div>
         </div>
@@ -16,20 +16,40 @@ function WhoAmI() {
 
 function CodeBlock() {
     return (
-        <div>
-            <p><span className='text-purple-400'>package</span> main</p>
-            <br />
-            <p>type Engineer struct {`{`}</p>
-            <p className='ml-4'>name string</p>
-            <p className='ml-4'>location string</p>
-            <p className='ml-4'>role string</p>
+        <div className='text-neutral-400'>
+            <p>
+            <span className="text-purple-400">package</span> <span className="text-white">main</span>
+            </p>
+            <br/>
+            <p>
+            <span className="text-purple-400">type</span> <span className="text-blue-400">Engineer</span> <span className="text-purple-400">struct</span> {`{`}
+            </p>
+            <p className="ml-4">
+            <span className="text-neutral-400">name</span> <span className="text-white">string</span>
+            </p>
+            <p className="ml-4">
+            <span className="text-neutral-400">location</span> <span className="text-white">string</span>
+            </p>
+            <p className="ml-4">
+            <span className="text-neutral-400">role</span> <span className="text-white">string</span>
+            </p>
             <p>{`}`}</p>
-            <br />
-            <p>func NewEngineer() Engineer {`{`}</p>
-            <p className="ml-4">return Engineer{`{`}</p>
-            <p className="ml-8">name: "Yash Arya",</p>
-            <p className="ml-8">location: "Bangalore, India",</p>
-            <p className="ml-8">role: "Backend Engineer",</p>
+            <br/>
+            <p>
+            <span className="text-purple-400">func</span> <span className="text-blue-400">NewEngineer</span>() Engineer {`{`}
+            </p>
+            <p className="ml-4">
+            <span className="text-purple-400">return</span> Engineer{`{`}
+            </p>
+            <p className="ml-8">
+            <span className="text-neutral-400">name:</span> <span className="text-green-400">"Yash Arya"</span>,
+            </p>
+            <p className="ml-8">
+            <span className="text-neutral-400">location:</span> <span className="text-green-400">"Bengaluru, India"</span>,
+            </p>
+            <p className="ml-8">
+            <span className="text-neutral-400">role:</span> <span className="text-green-400">"Backend Engineer"</span>,
+            </p>
             <p className="ml-4">{`}`}</p>
             <p>{`}`}</p>
             <br />
